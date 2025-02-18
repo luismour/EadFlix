6cf6d67a2aeff7d8033ca1058c1f83da7139244f
Luismour <luismsantos.contato@gmail.com>
2025-02-17 22:07:49 -0300

    feat-create-episode-table

---
+++ src/adminjs/resources/episode.ts
@@ -0,0 +1,11 @@
+import { ResourceOptions } from "adminjs";
+
+export const episodeResourceOptions: ResourceOptions = {
+    navigation: 'Catálogo',
+    editProperties: ['name', 'synopsis', 'courseId', 'order', 'uploadVideo', 'secondsLong'],
+    filterProperties: ['name', 'synopsis', 'courseId', 'secondsLong', 'createdAt', 'updatedAt'],
+    listProperties: ['id', 'name', 'courseId', 'order', 'secondsLong' ],
+    showProperties: ['id', 'name', 'synopsis', 'courseId', 'order', 'videoUrl', 'secondsLong', 'createdAt', 'updateAt']
+
+}
+
