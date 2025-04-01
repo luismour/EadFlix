1efbea4b79affcbaf64f31b0fc03fc45a7874eea
Luismour <luismsantos.contato@gmail.com>
2025-04-01 13:13:25 -0300

    feat: upload-thumbnails

--- src/adminjs/resources/course.ts
+++ src/adminjs/resources/course.ts
@@ -1,6 +1,8 @@
 // src/adminjs/resources/course.ts
 
-import { ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path";
 
 export const courseResourceOptions: ResourceOptions = {
   navigation: 'Catálogo',
@@ -10,3 +12,17 @@ export const courseResourceOptions: ResourceOptions = {
   showProperties: ['id', 'name', 'synopsis', 'featured', 'thumbnailUrl', 'categoryId', 'createdAt', 'updatedAt']
 }
 
+export const courseResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, '../../../public')
+      }
+    },
+    properties: {
+      key: 'thumbnailUrl',
+      file: 'uploadThumbnail'
+    },
+    uploadPath: (record, filename) => `thumbnails/course-${record.get('id')}/${filename}`
+  })
+]
