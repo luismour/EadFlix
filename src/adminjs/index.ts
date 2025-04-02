cf60d0f8b761c6ea10156f293a3f9ab1304258fa
Luismour <luismsantos.contato@gmail.com>
2025-04-01 23:57:54 -0300

    feat: tela-login-and-autenticacao

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,6 +3,9 @@ import AdminJSExpress from "@adminjs/express"
 import AdminJSSequelize from "@adminjs/sequelize"
 import { sequelize } from "../database"
 import { adminJsResources } from "./resources"
+import { User } from "../models"
+import bcrypt from 'bcrypt'
+require('dotenv').config()
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -34,4 +37,21 @@ export const adminJs = new AdminJS({
     })
     
 
-export const adminJsRouter = AdminJSExpress.buildRouter(adminJs)
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+  authenticate: async (email, password) => {
+      const user = await User.findOne({ where: {email} })
+
+      if (user && user.role === 'admin') {
+        const matched = await bcrypt.compare(password, user.password)
+
+        if(matched) {
+          return user
+        }
+      }
+      return false
+  },
+  cookiePassword: 'process.env.COOKIE',
+}, null, {
+  resave: false,
+  saveUninitialized: false
+})
