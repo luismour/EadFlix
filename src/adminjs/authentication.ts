264830d67d4dfc2c50d3e7b52d0e07bd39070434
Luismour <luismsantos.contato@gmail.com>
2025-04-08 21:12:21 -0300

    refactor: organização-de-arquivos

---
+++ src/adminjs/authentication.ts
@@ -0,0 +1,19 @@
+import { AuthenticationOptions } from "@adminjs/express";
+import { User } from "../models/User";
+import bcrypt from 'bcrypt'
+
+export const authenticateOptions: AuthenticationOptions = {
+    authenticate: async (email, password) => {
+        const user = await User.findOne({ where: {email} })
+  
+        if (user && user.role === 'admin') {
+          const matched = await bcrypt.compare(password, user.password)
+  
+          if(matched) {
+            return user
+          }
+        }
+        return false
+    },
+    cookiePassword: 'process.env.COOKIE'
+}
