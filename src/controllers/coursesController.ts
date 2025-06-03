30543ed258a2675d34d4c4737f81ece5d33b761b
Luismour <luismsantos.contato@gmail.com>
2025-06-03 20:50:50 -0300

    feat: create-findByName-courses

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import { courseService } from '../services/courseService'
+import { getPaginationParams } from '../helpers/getPaginationParams'
+
 
 export const coursesController = {
 
@@ -16,7 +18,35 @@ export const coursesController = {
             }
         }
     },
-    // GET /courses
+
+        // GET /courses/newest
+    newest: async (req: Request, res: Response) => {
+        try {
+            const newestCourses = await courseService.getTopNewest()
+            return res.json(newestCourses)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+    // GET /courses/search?name=some-name
+    search : async (req: Request, res: Response) => {
+        const { name } = req.query 
+        const [page, perPage] = getPaginationParams(req.query)
+
+        try {
+            if (typeof name !== 'string') throw new Error('Invalid name parameter');
+            const courses = await courseService.findByName(name, page, perPage)
+            return res.json(courses)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+
+    // GET /courses/:id
     show: async (req: Request, res: Response) => {
         const { id } = req.params
 
@@ -31,14 +61,4 @@ export const coursesController = {
         }
     },
 
-    newest: async (req: Request, res: Response) => {
-        try {
-            const newestCourses = await courseService.getTopNewest()
-            return res.json(newestCourses)
-        } catch (error) {
-            if (error instanceof Error) {
-                return res.status(400).json({ message: error.message })
-            }
-        }
-    }
 }
