855450bb27d7b3be189d8708302ce32f23b7ebf9
Luismour <luismsantos.contato@gmail.com>
2025-06-12 11:47:49 -0300

    update: credentials

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,8 +5,8 @@ export const sequelize = new Sequelize({
     dialect: 'postgres',
     host: process.env.DB_HOST,
     port: 5432,
-    database: process.env.DB_DATABASE,
-    username: 'postgres',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     define: {
         underscored: true
